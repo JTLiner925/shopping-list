299b27e2be1fe08f61323542fad8226561c3ec1d
JT Liner <jtliner925@gmail.com>
2020-02-10 11:19:36 -0600

    added index.js and linked to cdn to host jquery

---
+++ index.js
@@ -0,0 +1,6 @@
+//enter items needed by EVENT clicking "add item" button
+
+//check and uncheck items by EVENT clicking "check" button
+
+//remove items from list by EVENT clicking "delete" button
+
