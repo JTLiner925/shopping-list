dd0f79aba6d28df5ffa1e63916a05bcb6c5283e5
JT Liner <jtliner925@gmail.com>
2020-02-10 12:33:54 -0600

    working on 'add item' function

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+'use strict';
 //enter items needed by EVENT clicking "add item" button
+$(function(){
+  $('#js-shopping-list-form').submit(event =>{
+    event.preventDefault();
+
+    const newItem =  $('#js-shopping-list-entry').val();
+    $('#shopping-list-entry').val('');
+    console.log(newItem);
+    
+
+    $('.shopping-list').append(`
+    <li>
+        <span class="shopping-item">${newItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+  });
+  
+});
 
 //check and uncheck items by EVENT clicking "check" button
+$(function(){
 
+});
 //remove items from list by EVENT clicking "delete" button
+$(function(){
 
+});
